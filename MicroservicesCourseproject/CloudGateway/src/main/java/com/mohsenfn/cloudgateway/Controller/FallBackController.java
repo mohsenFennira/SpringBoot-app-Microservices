c102772736216970547d539c4678da8180fe79c2
mohsenfennira <mohsen.fennira@esprit.tn>
2023-07-09 07:37:06 +0100

    implementing circuit breaker in API Gateway(if services down)

---
+++ MicroservicesCourseproject/CloudGateway/src/main/java/com/mohsenfn/cloudgateway/Controller/FallBackController.java
@@ -0,0 +1,20 @@
+package com.mohsenfn.cloudgateway.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackController {
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallback(){
+        return "Order Service is down!";
+    }
+    @GetMapping("/productServiceFallBack")
+    public String productServiceFallback(){
+        return "product Service is down!";
+    }
+    @GetMapping("/paymentServiceFallBack")
+    public String paymentServiceFallback(){
+        return "payment Service is down!";
+    }
+}
