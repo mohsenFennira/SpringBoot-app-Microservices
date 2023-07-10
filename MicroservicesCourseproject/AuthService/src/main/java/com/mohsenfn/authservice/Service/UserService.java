07dafb9cc4a59d3df87830cdbe36a013553a5da3
mohsenfennira <mohsen.fennira@esprit.tn>
2023-07-10 14:46:54 +0100

    add user +Password encoder

--- MicroservicesCourseproject/AuthService/src/main/java/com/mohsenfn/authservice/Service/UserService.java
+++ MicroservicesCourseproject/AuthService/src/main/java/com/mohsenfn/authservice/Service/UserService.java
@@ -3,14 +3,18 @@ package com.mohsenfn.authservice.Service;
 import com.mohsenfn.authservice.Entity.User;
 import com.mohsenfn.authservice.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserIService {
     @Autowired
     UserRepository ur;
+    @Autowired
+    PasswordEncoder passwordEncoder;
     @Override
     public String addUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         ur.save(user);
         return "user added";
     }
